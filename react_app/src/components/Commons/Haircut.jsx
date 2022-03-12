1068f84a26bb40bd7c308b372f92512b818776ce
yeeahG <yeji.sep.k@gmail.com>
2022-03-12 16:02:02 +0900

    [upload]hair row 추기, 정렬

--- react_app/src/components/Commons/Haircut.jsx
+++ react_app/src/components/Commons/Haircut.jsx
@@ -11,30 +11,26 @@ const Haircut = (props) => {
 
     const hair = {
       id: props.id,
+      image: props.image,
       name: props.name,
       price: props.price
     }
-    console.log(hair.id);
- 
+    console.log(hair);
     hairContext.addItem(hair);
   }
 
 
   return (
-    <>
-      <div className='hairContent'>
-        <Button type='submit' onClick={submitHandler}>
-          <img src="https://images.unsplash.com/photo-1587776535733-b4c80a99ef82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=341&q=80"/>
-        </Button>
-        <div className='styleText'>
-          <div className='hairName'>{props.name}</div>
-          <div className='hairPrice'>{props.price}</div>
-        </div>    
-        
+    <div className='hairContent'>
+      <Button type='submit' onClick={submitHandler}>
+        {/* <img src="https://images.unsplash.com/photo-1587776535733-b4c80a99ef82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=341&q=80"/> */}
+        <img className="hairImage" src={props.image}/>
+      </Button>
+      <div className='styleText'>
+        <div className='hairName'><h1>{props.name}</h1></div>
+        <div className='hairPrice'>{props.price}</div>
       </div>
-
-    </>
-    
+    </div>
   )
 }
 
--- react_app/src/components/HairStyle/HairStyle.jsx
+++ react_app/src/components/HairStyle/HairStyle.jsx
@@ -5,35 +5,37 @@ import HairContext from '../Store/HairContext'
 import './HairStyle.css'
 
 
-// const hairButton=document.querySelector('.hairBox')
-// console.log(hairButton);
-
 const hairDesign = [
     {
         id: "cut1",
-        name: "cut1",
-        price: 10.57,
+        image: "https://images.unsplash.com/photo-1615363001828-acfd7ac403cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        name: "여성 커트일반",
+        price: 2.5,
     },
     {
         id: "cut2",
-        name: "cut2",
-        price: 12.97,
+        image: "https://images.unsplash.com/photo-1587776535733-b4c80a99ef82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=341&q=80",
+        name: "남성 커트일반",
+        price: 2.5,
     },
     {
         id: "cut3",
-        name: "cut3",
-        price: 15.55,
+        image: "https://images.unsplash.com/photo-1617896848219-5ec29570d680?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        name: "염색",
+        price: 2.2,
     },
 
     {
         id: "cut4",
-        name: "cut4",
-        price: 16.92,
+        image: "https://images.unsplash.com/photo-1620122830785-a18b43585b44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        name: "드라이",
+        price: 3.5,
     },
     {
         id: "cut5",
-        name: "cut5",
-        price: 15.92,
+        image: "https://images.unsplash.com/photo-1629617375711-8f591cfa357e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
+        name: "펌",
+        price: 5.5,
     }
 ];
 
@@ -45,36 +47,18 @@ const HairStyle = () => {
         <Haircut
             key={hair.id}
             id={hair.id}
+            image={hair.image}
             name={hair.name}
             price={hair.price}
         />
         )
-
-    // const hairValue = document.getElementById('name');
-    // console.log(hairValue);
-
-    // const hairContext = useContext(HairContext);
-    
-    // const submitHandler = (name) => {
-    //     //event.preventDefault();
-    //     console.log(name);
-
-    //     const hair = {
-    //         id: props.id,
-    //         name: props.name,
-    //         price: props.price
-    //     }
-        
-    //     hairContext.addItem(hair);
-    // }
-
    
     return (
-        <div>
-        {/* <div className='hairContent'> */}
-            <>
+        <div className='box'>
+            <div className='hairContainer'>
+            {/* <div className='hairContainer'> */}
                 {hairList}
-            </>
+            </div>
         </div>
     )
 }
